b74537ea00aa96f9904f1efacaf0ba02bde19c4d
unknown <sd979141@gmail.com>
2023-02-18 17:14:46 +0900

    color-pixer

---
+++ main.js
@@ -0,0 +1,48 @@
+const body = document.querySelector('body')
+const container = document.querySelector('.btn-main')
+const colorText = document.querySelector('.hex')
+const values = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+]
+
+// random color function
+function getGradient() {
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.floor(Math.random() * values.length)
+    color += values[randomNumber]
+  }
+  return color
+}
+
+// implement color
+function setGradient() {
+  const color1 = getGradient()
+  const color2 = getGradient()
+  const randomDeg = Math.floor(Math.random() * 360)
+  const bgColor = `linear-gradient(
+    ${randomDeg}deg,
+    ${color1},
+    ${color2}
+  )`
+  body.style.background = bgColor
+
+  colorText.textContent = bgColor
+}
+setGradient()
+container.addEventListener('click', setGradient)
